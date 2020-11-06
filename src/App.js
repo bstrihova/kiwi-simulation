04a75b41be34b0c99526b3e0cc00cd31d4583290
Eva Sustekova <eva.sustekova@gmail.com>
2020-11-06 10:07:06 +0100

    api setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+
+
+  const App = () => {
+      // const [searchValue, setSearchValue] = React.useState();
+      // const [searchResults, setSearchResults] = React.useState();
+    
+      // React.useEffect(() => {
+      //   fetchData();
+      // }, [searchValue])
+
+      const [flyFrom, setFlyFrom ] = React.useState("PRG");
+      const [flyTo, setFlyTo ] = React.useState("LGW");
+      const [dateFrom, setDateFrom ] = React.useState("18/11/2020");
+      const [dateTo, setDateTo ] = React.useState("12/12/2020");
+
+       React.useEffect(() => {
+       fetchData();
+       })   
+    
+      const fetchData = async () => {
+        const url = `https://api.skypicker.com/flights?flyFrom=${flyFrom}&to=${flyTo}&dateFrom=${dateFrom}&dateTo=${dateTo}&partner=picky&v=3`;
+        const response = await fetch(url);
+        console.log(response);
+      }
+    
+      return (
+        <div className="App">
+        
+        
+        {/* <SearchBar setSearchValue={setSearchValue} />
+        <SearchResult searchValue={searchValue} /> */}
+
+
+        </div>
+         )
+    }
+    
+    export default App;
+    
+    
+    
+    
+    
+    
+    
+    /* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,6 +64,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
-export default App;
+
